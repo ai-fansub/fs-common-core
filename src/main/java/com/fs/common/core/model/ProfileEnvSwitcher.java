31f2d0cc3c06167b6d0e4a0e3492c430820c0324
rmsoft-kai <hwangsh@rmsoft.kr>
2025-04-16 13:56:09 +0900

    [init]: Common-core 생성
    Fansub-Common-core 생성

---
+++ src/main/java/com/fs/common/core/model/ProfileEnvSwitcher.java
@@ -0,0 +1,8 @@
+package com.fs.common.core.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public final class ProfileEnvSwitcher {
+    @Getter @Setter private static String developEnv = "local";
+}
